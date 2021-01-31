6c77cdadbc7fc286b15fe647e7bbff52457dbb45
Harpo Jaeger <harpo.jaeger@gmail.com>
2021-01-31 17:35:45 -0500

    write jsdoc

--- bytesize.js
+++ bytesize.js
@@ -1,3 +1,13 @@
+/**
+ *
+ * @param {int} bytes An integer number of bytes. May be zero or negative.
+ * @param {int} maxDecimalDigits The maximum number of digits after the decimal
+ * place in the returned, human-readable string. This is not equivalent to
+ * significant figures; if the computed value has fewer digits after formatting,
+ * it will not be zero-padded.
+ * @return {string} A human-readable bytesize expression, such as '10G' or
+ * '-3.5M'.
+ */
 function bytesize(bytes, maxDecimalDigits) {
   return '';
 }
